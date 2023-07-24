3854370d1206885e0365567adf1edc8c690be7d7
An Seo-young <asy047@naver.com>
2023-07-24 17:15:47 +0900

    test

---
+++ constants/style.js
@@ -0,0 +1,16 @@
+export const GlobaStyles = {
+  colors: {
+    primaryColor: '#39E3B3',
+    secondColor: '#3CCBCB',
+    highlightColor: '#FF4333',
+    whiteColor: '#ffffff',
+    blackColor: '#222222',
+    fontsizeDefault: 16,
+  },
+  padding: {
+    sidePadding: 5,
+  },
+  fontsize: {
+    defaultSize: 16,
+  },
+};
