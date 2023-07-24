5695c67c23bf2d03a44c4580965c2bc445298ae5
An Seo-young <asy047@naver.com>
2023-07-24 15:57:19 +0900

    최초 commit

---
+++ components/Home.tsx
@@ -0,0 +1,104 @@
+import React from 'react';
+import { Text, View, Image, StyleSheet } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+
+const Home = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageArea}>
+        <Image
+          source={require('../assets/homeImage.png')}
+          style={{ width: '100%' }}
+        ></Image>
+      </View>
+      <View style={styles.bgBox}>
+        <View style={styles.chargeArea}>
+          <LinearGradient
+            colors={['#39E3B3', '#3CCBCB']} // 그라디언트 색상 배열
+            style={styles.gradient}
+            start={{ x: 1, y: 0 }} // 시작점 (왼쪽 아래)
+            end={{ x: 0, y: 1 }} // 끝점 (오른쪽 위)
+          >
+            <View style={styles.chargeCircle}>
+              {/* <LinearGradient
+                colors={['#39E3B3', '#3CCBCB']} // 그라디언트 색상 배열
+                start={{ x: 1, y: 1 }}
+                end={{ x: 1, y: 0 }}
+                style={{ borderRadius: 8, overflow: 'hidden' }}
+              > */}
+              <Text style={styles.chargePercent}>100%</Text>
+              {/* </LinearGradient> */}
+              <Text>충전 완료!</Text>
+            </View>
+          </LinearGradient>
+        </View>
+        <View style={styles.recentParkBox}>
+          <Text>현재 주차 위치</Text>
+          <Text>B4 3번</Text>
+          <Text>12하 5678</Text>
+        </View>
+        <View>
+          <View>
+            <Text>배터리 온도</Text>
+            <Text>주의</Text>
+          </View>
+          <View>
+            <Text>유해가스</Text>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  imageArea: {
+    position: 'relative',
+    backgroundColor: 'blue',
+  },
+  bgBox: {
+    width: '100%', // 화면 전체로 설정
+    backgroundColor: 'white',
+    borderTopRightRadius: 50,
+    borderTopLeftRadius: 50,
+    alignItems: 'center',
+  },
+  chargeArea: {
+    width: 230,
+    height: 230,
+    zIndex: 1,
+    position: 'absolute',
+    top: -60,
+  },
+  gradient: {
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 230,
+  },
+  chargeCircle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ffffff',
+    width: '96%',
+    height: '96%',
+    borderRadius: 230,
+  },
+  chargePercent: {
+    fontSize: 36,
+    fontWeight: 'bold',
+    color: '#39E3B3',
+    overflow: 'hidden',
+  },
+  recentParkBox: {
+    flex: 1,
+    backgroundColor: 'red',
+  },
+});
+
+export default Home;
