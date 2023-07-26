93c2351fb9946564bb465b8520132bf4b2a09cd6
An Seo-young <asy047@naver.com>
2023-07-27 02:19:40 +0900

    나는 널 좋아하지만 좋아하지 않는척을 할거야

--- App.tsx
+++ App.tsx
@@ -1,12 +1,39 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import Home from './components/Home';
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
+import { Welcome, Signup, Login } from "./screens";
+import Home from "./components/Home";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Home />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Welcome">
+        <Stack.Screen
+          name="Welcome"
+          component={Welcome}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="Signup"
+          component={Signup}
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ screens/index.tsx
@@ -0,0 +1,5 @@
+import Welcome from "./Welcome";
+import Login from "./Login";
+import Signup from "./Signup";
+
+export { Welcome, Login, Signup };
