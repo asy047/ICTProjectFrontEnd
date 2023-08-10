1950202d9eee6194bc8c0964f3b12d889add556c
An Seo-young <asy047@naver.com>
2023-08-10 17:52:11 +0900

    대충 다 만들었고 Home 구축 중

--- App.tsx
+++ App.tsx
@@ -2,16 +2,46 @@ import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+// import axios from "axios";
 
 const Stack = createNativeStackNavigator();
 import { Welcome, Signup, Login } from "./screens";
-import Home from "./components/Home";
+import Home from "./screens/Home";
 import COLORS from "./constants/colors";
 
+// const apiUrl = "http://localhost:8080/";
+
+// axios.get(`${apiUrl}/endpoint`)
+//   .then(response => {
+//     console.log(response.data); // 백엔드에서 받아온 데이터 처리
+//   })
+//   .catch(error => {
+//     console.error('Error fetching data:', error);
+//   });
+
+// // POST 요청 예시
+// const requestData = { /* 요청 데이터 */ };
+// axios.post(`${apiUrl}/endpoint`, requestData)
+//   .then(response => {
+//     console.log(response.data); // 백엔드에서 받아온 응답 처리
+//   })
+//   .catch(error => {
+//     console.error('Error posting data:', error);
+//   });
+
 export default function App() {
+  const homeis = false;
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Welcome">
+      <Stack.Navigator initialRouteName={homeis ? "Home" : "Welcome"}>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            headerShown: false,
+            headerBackTitleVisible: false,
+          }}
+        />
         <Stack.Screen
           name="Welcome"
           component={Welcome}
