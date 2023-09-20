366c824225de21f91b1afa7aba4eae5107c7ae9b
An Seo-young <asy047@naver.com>
2023-09-20 14:19:14 +0900

    deleted all

--- arduino/Motor_False.h
+++
@@ -1,9 +0,0 @@
-#ifndef MOTOR_FALSE_H
-#define MOTOR_FALSE_H
-
-#include <Servo.h>
-
-// 역방향 모터 동작 함수
-void motorFalse(Servo &servo);
-
-#endif
--- arduino/Motor_True.cpp
+++
@@ -1,6 +0,0 @@
-#include "Motor_True.h"
-
-void motorTrue(Servo &servo) {
-  servo.write(80);  // 정방향 모터 동작
-  delay(1500);  // 1.5초 대기
-}
